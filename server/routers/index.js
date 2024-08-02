99621f477b5dcbae338765fcf4e6bfb36f684a95
muhammadsubhan14 <muhammadsubhantarmedi@gmail.com>
2024-08-03 00:33:57 +0700

    feat: authen

--- server/routers/index.js
+++ server/routers/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const userRouter = require("./user");
 const newsRouter = require("./news");
+// const authentication = require("../middlewares/authentication");
 
 router.use(userRouter);
+
+// router.use(authentication)  
 router.use(newsRouter)
 
 module.exports = router;
--- server/routers/news.js
+++ server/routers/news.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const Controller = require("../controllers/controller");
+const authentication = require("../middlewares/authentication");
 const news = express.Router();
+
+// news.get("/news", authentication, Controller.getAllNews);
+// news.get("/news/:id", authentication, Controller.getNewsById);
+// news.post("/news", authentication, Controller.createNews);
+// news.delete("/news/:id", authentication, Controller.deleteNews);
 
 news.get("/news", Controller.getAllNews);
 news.get("/news/:id", Controller.getNewsById);
 news.post("/news", Controller.createNews);
 news.delete("/news/:id", Controller.deleteNews);
 
-module.exports = news
+
+module.exports = news;
